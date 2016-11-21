4ff029ab5e29ff4334b38f528a0c360aaafbb285
lintsai <lin15642@gmail.com>
2016-11-21 15:47:49 +0800

    20161121

--- Info360/src/com/infotrends/servlet/Update_PersonInfoServlet.java
+++ Info360/src/com/infotrends/servlet/Update_PersonInfoServlet.java
@@ -1,7 +1,6 @@
 package com.infotrends.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
@@ -11,7 +10,6 @@ import javax.ws.rs.core.Response;
 
 import org.json.JSONObject;
 
-import com.infotrends.bean.CFG_group_person;
 import com.infotrends.bean.CFG_person;
 import com.infotrends.service.MaintainService;
 import com.infotrends.util.IsError;
@@ -39,8 +37,8 @@ public class Update_PersonInfoServlet {
 			@FormParam("last_name") String last_name,
 			@FormParam("user_name") String user_name,
 			@FormParam("emailaddress") String emailaddress,
-			@FormParam("password") String password,
-			@FormParam("group_dbid") long group_dbid
+			@FormParam("password") String password
+			//@FormParam("group_dbid") long group_dbid
 			) throws IOException {
 		
 
@@ -58,14 +56,14 @@ public class Update_PersonInfoServlet {
 			MaintainService maintainService = new MaintainService();
 			updatecount = maintainService.update_PersonInfo(cfg_person);
 			jsonObject.put("updatecount", updatecount);
-			if(group_dbid!=0){
-				List<CFG_person> cfg_personlist = maintainService.query_Person_Account(cfg_person);
-				CFG_group_person cfg_group_person = new CFG_group_person();
-				cfg_group_person.setPerson_dbid(cfg_personlist.get(0).getDbid());
-				cfg_group_person.setGroup_dbid(group_dbid);
-				int grouppersoncount = maintainService.update_Group_PersonInfo(cfg_group_person);
-				jsonObject.put("group_person_insertcount", grouppersoncount);
-			}
+//			if(group_dbid!=0){
+//				List<CFG_person> cfg_personlist = maintainService.query_Person_Account(cfg_person);
+//				CFG_group_person cfg_group_person = new CFG_group_person();
+//				cfg_group_person.setPerson_dbid(cfg_personlist.get(0).getDbid());
+//				cfg_group_person.setGroup_dbid(group_dbid);
+//				int grouppersoncount = maintainService.update_Group_PersonInfo(cfg_group_person);
+//				jsonObject.put("group_person_insertcount", grouppersoncount);
+//			}
 			
 		} catch (Exception e) {
 			if(IsError.GET_EXCEPTION != null)
